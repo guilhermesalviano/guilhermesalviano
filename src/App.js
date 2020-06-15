42819f8f81b093d365659a87b83010f4d80f2d83
Guilherme <guilherme@MacBook.local>
2020-06-15 00:44:43 -0300

    end version 1

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import TypeIt from "typeit-react";
+import { FaGithub, FaLinkedinIn, FaInstagram } from "react-icons/fa";
+import imgPerfil from './images/perfil.jpeg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <img src={imgPerfil} className="App-profile" alt="Eu" />
+        <div id="App-Text">
+          <TypeIt
+            getBeforeInit={instance => {
+              instance
+                .type("Olá, eu sou o Guibs!")
+                .pause(750)
+                .delete(3)
+                .pause(300)
+                .type("lherme!")
+                .pause(500)
+                .type("<br />E sou um profissional de Marketing digital e Desenvolvedor nas horas vagas.");
+              return instance;
+            }}
+          />
+        </div>
+        <div className="App-Contacts">
+          <ul className="contacts-list">
+            <li className="contacts-item">
+              <a target="_blank" href="https://github.com/Guibs1/Projects">
+                <FaGithub size={52} color="#fff" />
+              </a>
+            </li>
+            <li className="contacts-item">
+              <a target="_blank" href="https://www.linkedin.com/in/guilherme-salviano/">
+                <FaLinkedinIn size={52} color="#fff" />
+              </a>
+            </li>
+            <li className="contacts-item">
+              <a target="_blank" href="https://www.instagram.com/elguibs_/">
+                <FaInstagram size={52} color="#fff" />
+              </a>
+            </li>
+          </ul>
+        </div>
+    </div>
+  );
+}
+
+export default App;
