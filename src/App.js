93a760808283fb33b71ca9feb7be7157530d89e8
Guibs <guilherme.salviano12@outlook.com>
2021-04-02 15:13:36 -0300

    adding nav and remove image in home

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {
 	FaLinkedinIn,
 	FaInstagram,
 	FaWhatsapp,
+	FaAngleDown,
 } from "react-icons/fa";
 import imgPerfil from "./images/perfil.jpeg";
 import "./App.css";
@@ -12,63 +13,78 @@ import "./App.css";
 function App() {
 	return (
 		<div className="App">
-			<img src={imgPerfil} className="App-profile" alt="Eu" />
-			<div id="App-Text">
-				<TypeIt
-					getBeforeInit={(instance) => {
-						instance
-							.type("Olá, eu sou o Guibs!")
-							.pause(750)
-							.delete(3)
-							.pause(300)
-							.type("lherme!")
-							.pause(500)
-							.type(
-								'<br /><span class="App-Text-Description">E sou um profissional de Marketing digital e Desenvolvedor nas horas vagas.</span>'
-							);
-						return instance;
-					}}
-				/>
-			</div>
-			<div className="App-Contacts">
-				<ul className="contacts-list">
-					<li className="contacts-item">
-						<a
-							target="_blank"
-							rel="noopener noreferrer"
-							href="https://github.com/guilhermesalviano/Projects"
-						>
-							<FaGithub size={52} color="#fff" />
-						</a>
-					</li>
-					<li className="contacts-item">
-						<a
-							target="_blank"
-							rel="noopener noreferrer"
-							href="https://www.linkedin.com/in/guilherme-salviano/"
-						>
-							<FaLinkedinIn size={52} color="#fff" />
-						</a>
-					</li>
-					<li className="contacts-item">
-						<a
-							target="_blank"
-							rel="noopener noreferrer"
-							href="https://www.instagram.com/guilhermesalvian0"
-						>
-							<FaInstagram size={52} color="#fff" />
-						</a>
-					</li>
-					<li className="contacts-item">
-						<a
-							target="_blank"
-							rel="noopener noreferrer"
-							href="https://api.whatsapp.com/send?1=pt_BR&phone=5511948449969&text=Olá+Guilherme,+encontrei+seu+número+pelo+seu+site+e+"
-						>
-							<FaWhatsapp size={52} color="#fff" />
-						</a>
-					</li>
+			<div className="nav-header">
+				<ul>
+					<li>Home</li>
+					<li>Sobre</li>
+					<li>Serviços</li>
+					<li>Portfólio</li>
+					<li>Blog</li>
+					<li>Contatos</li>
 				</ul>
+			</div>
+			<div className="App-Content">
+				<img src={imgPerfil} className="App-profile" alt="Eu" />
+				<div id="App-Text">
+					<TypeIt
+						getBeforeInit={(instance) => {
+							instance
+								.type("Olá, eu sou o Guibs!")
+								.pause(750)
+								.delete(3)
+								.pause(300)
+								.type("lherme!")
+								.pause(500)
+								.type(
+									'<br /><span class="App-Text-Description">E sou um profissional de Marketing digital e Desenvolvedor nas horas vagas.</span>'
+								);
+							return instance;
+						}}
+					/>
+				</div>
+				<div className="App-Contacts">
+					<ul className="contacts-list">
+						<li className="contacts-item">
+							<a
+								target="_blank"
+								rel="noopener noreferrer"
+								href="https://github.com/guilhermesalviano/Projects"
+							>
+								<FaGithub size={52} color="#fff" />
+							</a>
+						</li>
+						<li className="contacts-item">
+							<a
+								target="_blank"
+								rel="noopener noreferrer"
+								href="https://www.linkedin.com/in/guilherme-salviano/"
+							>
+								<FaLinkedinIn size={52} color="#fff" />
+							</a>
+						</li>
+						<li className="contacts-item">
+							<a
+								target="_blank"
+								rel="noopener noreferrer"
+								href="https://www.instagram.com/guilhermesalvian0"
+							>
+								<FaInstagram size={52} color="#fff" />
+							</a>
+						</li>
+						<li className="contacts-item">
+							<a
+								target="_blank"
+								rel="noopener noreferrer"
+								href="https://api.whatsapp.com/send?1=pt_BR&phone=5511948449969&text=Olá+Guilherme,+encontrei+seu+número+pelo+seu+site+e+"
+							>
+								<FaWhatsapp size={52} color="#fff" />
+							</a>
+						</li>
+					</ul>
+				</div>
+				<div className="App-Scroll-Button">
+					<FaAngleDown size={52} color="#fff" />
+				</div>
 			</div>
 		</div>
 	);
---
+++ src/Components/About/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import 'styles.css';
+
+const About = () => {
+    return <div />
+}
+
+export default About;
