ac34f0fc26a9320f257c3f887c455804a346f6d5
Guibs <guilherme.salviano12@outlook.com>
2021-04-03 16:14:04 -0300

    adding more projects in component portifolio

--- src/Components/Portifolio/index.js
+++ src/Components/Portifolio/index.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import gigprintImg from "../../images/gigprint_telas.jpg";
+import aprendaAprenderImg from "../../images/aprenda-a-aprender-telas.jpg";
+import studioElimarImg from "../../images/prints_studio_elimar.jpg";
 import './styles.css';
 
 const Portifolio = () => {
@@ -29,7 +31,23 @@ const Portifolio = () => {
                     <img src={gigprintImg} alt="GIGPrint" width="320" onClick={() => zoomImg(!modal, gigprintImg)}/>
                 </div>
                 <div className="app-portifolio">
-                    <img src={gigprintImg} alt="GIGPrint" width="320" onClick={() => zoomImg(!modal, gigprintImg)}/>
+                    <img src={aprendaAprenderImg} alt="GIGPrint" width="320" onClick={() => zoomImg(!modal, aprendaAprenderImg)}/>
+                </div>
+            </div>
+            <div className="app-portifolio-container">
+                <div className="app-portifolio">
+                    <img src={studioElimarImg} alt="GIGPrint" width="320" onClick={() => zoomImg(!modal, studioElimarImg)}/>
+                </div>
+                <div className="app-portifolio">
+                    <img src="https://github.com/Guibs1/ecoleta/raw/master/ecoleta-web.gif" alt="GIGPrint" height="195.5" onClick={() => zoomImg(!modal, "https://github.com/Guibs1/ecoleta/raw/master/ecoleta-web.gif")}/>
+                </div>
+            </div>
+            <div className="app-portifolio-container">
+                <div className="app-portifolio">
+                    <img src="https://github.com/Guibs1/to-be-hero/raw/master/to-be-hero-web.gif" alt="GIGPrint" height="255" onClick={() => zoomImg(!modal, "https://github.com/Guibs1/to-be-hero/raw/master/to-be-hero-web.gif")}/>
+                </div>
+                <div className="app-portifolio">
+                    <img src="https://raw.githubusercontent.com/Guibs1/messenger-copy/master/messenger_copy.gif" alt="GIGPrint" height="255" />
                 </div>
             </div>
         </div>
