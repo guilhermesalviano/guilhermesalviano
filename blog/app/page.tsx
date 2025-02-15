95b8efbead959063fa219452e5c39f4dc55566c3
Guilherme Salviano <guilherme.salviano12@outlook.com>
2025-02-15 16:37:57 -0300

    feat: creating blog

---
+++ blog/app/page.tsx
@@ -0,0 +1,20 @@
+import { BlogPosts } from 'app/components/posts'
+
+export default function Page() {
+  return (
+    <section>
+      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
+        Hi! My name is Guilherme,
+      </h1>
+      <p className="mb-4">
+        {`but you can call me Guibs. I graduated in Systems Development from ETEC and am currently studying Digital Games at Fatec. I’m passionate about technology, innovation, and solutions that positively impact people’s lives.`}
+      </p>
+      <p className="mb-4">
+        {`My career began as a CRM Apprentice, where I learned and grew. Since then, I’ve gained valuable experience as a Marketing Analyst and CRM Analyst (Mid and Senior levels) at Enext Consultoria, honing both technical and strategic skills along the way.`}
+      </p>
+      <p className="mb-4">
+        {`Let’s connect and create something amazing!`}
+      </p>
+    </section>
+  )
+}
