acd49058b64859565112aec8498173082019fff2
Guilherme Salviano <guilherme.salviano12@outlook.com>
2025-02-17 20:47:34 -0300

    feat(blog): adding profile

--- blog/app/page.tsx
+++ blog/app/page.tsx
@@ -1,18 +1,19 @@
-import Image from 'next/image'
 // import { BlogPosts } from 'app/components/posts'
 
 export default function Page() {
   return (
     <section>
-      <div className="flex items-center justify-between">
-        <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-          Hi! My name is Guilherme,
-        </h1>
-        <Image src="https://avatars.githubusercontent.com/u/36766110?v=4&size=48" width="48" height="48" alt="Profile" className="rounded-full" />
+      <div className="flex items-baseline flex-col-reverse md:flex-row justify-between">
+        <div>
+          <h1 className="my-6  text-2xl font-semibold tracking-tighter">
+            Hi! My name is Guilherme,
+          </h1>
+          <p className="mb-4">
+            {`but you can call me Guibs. I graduated in Systems Development from ETEC and am currently studying Digital Games at Fatec. I’m passionate about technology, innovation, and solutions that positively impact people’s lives.`}
+          </p>
+        </div>
+        <img src="https://avatars.githubusercontent.com/u/36766110?v=4&size=116" className="w-[116px] h-[116px] rounded-full m-auto object-cover" />
       </div>
-      <p className="mb-4">
-        {`but you can call me Guibs. I graduated in Systems Development from ETEC and am currently studying Digital Games at Fatec. I’m passionate about technology, innovation, and solutions that positively impact people’s lives.`}
-      </p>
       <p className="mb-4">
         {`My career began as a CRM Apprentice, where I learned and grew. Since then, I’ve gained valuable experience as a CRM Developer (Mid and Senior levels) at Enext Consultoria, honing both technical and strategic skills along the way.`}
       </p>
