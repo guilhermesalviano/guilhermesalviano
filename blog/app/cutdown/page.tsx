02015a0921f9b4fb110c3e85a29d9c5614da9a45
Guilherme Salviano <guilherme.salviano12@outlook.com>
2025-02-21 21:02:30 -0300

    fix: grid layout

--- blog/app/cutdown/page.tsx
+++ blog/app/cutdown/page.tsx
@@ -2,36 +2,22 @@ export default function page() {
     return (
         <div className="w-screen h-screen flex flex-col justify-center items-center text-center">
             <h1 className="text-4xl"><span className="text-purple-600 font-extrabold">JADE</span> IN SP</h1>
-            <div className="grid grid-cols-7 grid-rows-2 gap-4">
-                <div className="text-center align-middle text-neutral-900 max-md:text-xl max-sm:text-xl text-6xl pt-6 gap-4">
-                    <div>
-                        <h3 className="day flex text-center justify-center border-2 border-purple-400 rounded-2xl p-6">33</h3>
-                    </div>
-                    <div>
-                        <h3  className="dots flex text-center justify-center">:</h3>
-                    </div>
-                    <div>
-                        <h3 className="hour flex text-center justify-center border-2 border-purple-400 rounded-2xl p-6">00</h3>
-                    </div>
-                    <div>
-                        <h3  className="dots flex text-center justify-center">:</h3>
-                    </div>
-                    <div>
-                        <h3 className="minutes flex text-center justify-center border-2 border-purple-400 rounded-2xl p-6">00</h3>
-                    </div>
-                    <div>
-                        <h3  className="dots flex text-center justify-center">:</h3>
-                    </div>
-                    <div>
-                        <h3 className="secodes flex text-center justify-center border-2 border-purple-400 rounded-2xl p-6">00</h3>
-                    </div>
-                </div>
-                <div className="flex text-center items-center justify-center align-middle max-md:text-xl max-sm:text-xl text-2xl max-md:gap-10 gap-12">
-                    <p>Days</p>
-                    <p>Hours</p>
-                    <p>Minutes</p>
-                    <p>Secods</p>
-                </div>
+            
+            <div className="grid max-sm:grid-cols-[auto_14px_auto_14px_auto_14px_auto] grid-cols-[auto_40px_auto_40px_auto_40px_auto] grid-rows-2 justify-center max-sm:gap-0 max-sm:px-4 px-8 text-center align-middle text-neutral-900 max-sm:text-xl text-6xl pt-6">
+                <h3 className="day flex text-center justify-center border-2 border-purple-400 rounded-2xl p-6">33</h3>
+                <h3  className="flex text-center justify-center items-center pb-2">:</h3>
+                <h3 className="hour flex text-center justify-center border-2 border-purple-400 rounded-2xl p-6">00</h3>
+                <h3  className="flex text-center justify-center items-center pb-2">:</h3>
+                <h3 className="minutes flex text-center justify-center border-2 border-purple-400 rounded-2xl p-6">00</h3>
+                <h3  className="flex text-center justify-center items-center pb-2">:</h3>
+                <h3 className="secodes flex text-center justify-center border-2 border-purple-400 rounded-2xl p-6">00</h3>
+                <p className="text-2xl max-md:text-xl max-sm:text-xs">Days</p>
+                <div></div>
+                <p className="text-2xl max-md:text-xl max-sm:text-xs">Hours</p>
+                <div></div>
+                <p className="text-2xl max-md:text-xl max-sm:text-xs">Minutes</p>
+                <div></div>
+                <p className="text-2xl max-md:text-xl max-sm:text-xs">Seconds</p>
             </div>
         </div>
     )
