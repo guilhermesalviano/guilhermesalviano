e11ae79cf2d879b2931b0d0a412e466f8af5f928
Guilherme <guilherme.salviano12@outlook.com>
2025-11-18 21:57:08 -0300

    feat: add blank url

--- blog/app/components/projects.tsx
+++ blog/app/components/projects.tsx
@@ -1,34 +1,34 @@
-import { formatDate } from 'app/blog/utils'
-import Link from 'next/link'
+import { formatDate } from "app/blog/utils"
+import Link from "next/link"
 
 export function Projects() {
 
   return (
-    <div className='flex flex-col gap-3'>
-      <Link href={`https://koaris.me/?utm_source=portifolio&utm_medium=site&campaign=seemyprojects&utm_content=${new Date().toISOString().split("T")[0]}`}>
+    <div className="flex flex-col gap-3">
+      <Link target="_blank" href={`https://koaris.me/?utm_source=portifolio&utm_medium=site&campaign=seemyprojects&utm_content=${new Date().toISOString().split("T")[0]}`}>
         <div className="w-full flex max-sm:flex-col flex-row space-x-0 sm:space-x-2">
           <p className="text-neutral-600 dark:text-neutral-400 w-[160px] min-w-20 tabular-nums">
-            {formatDate('2025-05-03', false)} - Present
+            {formatDate("2025-05-03", false)} - Present
           </p>
           <p className="text-neutral-900 dark:text-neutral-100 tracking-tight">
             Koaris.me
           </p>
         </div>
       </Link>
-      <Link href={`https://koaris.com/?utm_source=portifolio&utm_medium=site&campaign=seemyprojects&utm_content=${new Date().toISOString().split("T")[0]}`}>
+      <Link target="_blank" href={`https://koaris.com/?utm_source=portifolio&utm_medium=site&campaign=seemyprojects&utm_content=${new Date().toISOString().split("T")[0]}`}>
         <div className="w-full flex max-sm:flex-col flex-row space-x-0 sm:space-x-2">
           <p className="text-neutral-600 dark:text-neutral-400 w-[160px] min-w-20 tabular-nums">
-            {formatDate('2025-02-24', false)} - {formatDate('2025-03-03', false)}
+            {formatDate("2025-02-24", false)} - {formatDate("2025-03-03", false)}
           </p>
           <p className="text-neutral-900 dark:text-neutral-100 tracking-tight">
             Marketing Tools
           </p>
         </div>
       </Link>
-      <Link target='_blank' href="https://github.com/koaris">
+      <Link target="_blank" href="https://github.com/koaris">
         <div className="w-full flex max-sm:flex-col flex-row space-x-0 sm:space-x-2">
           <p className="text-neutral-600 dark:text-neutral-400 w-[160px] min-w-20 tabular-nums">
-            {formatDate('2024-11-05', false)} - Present
+            {formatDate("2024-11-05", false)} - Present
           </p>
           <p className="text-neutral-900 dark:text-neutral-100 tracking-tight">
             Koaris Auth
@@ -38,87 +38,87 @@ export function Projects() {
       <Link href={`https://koaris.com.br/?utm_source=portifolio&utm_medium=site&campaign=seemyprojects&utm_content=${new Date().toISOString().split("T")[0]}`}>
         <div className="w-full flex max-sm:flex-col flex-row space-x-0 sm:space-x-2">
           <p className="text-neutral-600 dark:text-neutral-400 w-[160px] min-w-20 tabular-nums">
-            {formatDate('2023-08-05', false)} - Present
+            {formatDate("2023-08-05", false)} - Present
           </p>
           <p className="text-neutral-900 dark:text-neutral-100 tracking-tight">
             Koaris
           </p>
         </div>
       </Link>
-      <Link target='_blank' href="https://koaris.github.io/bloom-ui/">
+      <Link target="_blank" href="https://koaris.github.io/bloom-ui/">
         <div className="w-full flex max-sm:flex-col flex-row space-x-0 sm:space-x-2">
           <p className="text-neutral-600 dark:text-neutral-400 w-[160px] min-w-20 tabular-nums">
-            {formatDate('2023-12-04', false)} - Present
+            {formatDate("2023-12-04", false)} - Present
           </p>
           <p className="text-neutral-900 dark:text-neutral-100 tracking-tight">
             Design System - Bloom-ui
           </p>
         </div>
       </Link>
-      <Link target='_blank' href="https://github.com/guilhermesalviano/smaug-preco-do-poder">
+      <Link target="_blank" href="https://github.com/guilhermesalviano/smaug-preco-do-poder">
         <div className="w-full flex max-sm:flex-col flex-row space-x-0 sm:space-x-2">
           <p className="text-neutral-600 dark:text-neutral-400 w-[160px] min-w-20 tabular-nums">
-            {formatDate('2023-08-24', false)} - {formatDate('2023-11-27', false)}
+            {formatDate("2023-08-24", false)} - {formatDate("2023-11-27", false)}
           </p>
           <p className="text-neutral-900 dark:text-neutral-100 tracking-tight">
             Visual Novel - Preço do poder
           </p>
         </div>
       </Link>
-      <Link target='_blank' href="https://github.com/guilhermesalviano/ekko-project">
+      <Link target="_blank" href="https://github.com/guilhermesalviano/ekko-project">
         <div className="w-full flex max-sm:flex-col flex-row space-x-0 sm:space-x-2">
           <p className="text-neutral-600 dark:text-neutral-400 w-[160px] min-w-20 tabular-nums">
-            {formatDate('2022-04-01', false)} - {formatDate('2022-07-26', false)}
+            {formatDate("2022-04-01", false)} - {formatDate("2022-07-26", false)}
           </p>
           <p className="text-neutral-900 dark:text-neutral-100 tracking-tight">
             Platform game - Ekko
           </p>
         </div>
       </Link>
-      <Link target='_blank' href="https://github.com/guilhermesalviano">
+      <Link target="_blank" href="https://github.com/guilhermesalviano">
         <div className="w-full flex max-sm:flex-col flex-row space-x-0 sm:space-x-2">
           <p className="text-neutral-600 dark:text-neutral-400 w-[160px] min-w-20 tabular-nums">
-            {formatDate('2020-06-18', false)} - {formatDate('2021-10-20', false)}
+            {formatDate("2020-06-18", false)} - {formatDate("2021-10-20", false)}
           </p>
           <p className="text-neutral-900 dark:text-neutral-100 tracking-tight">
             Graphyk
           </p>
         </div>
       </Link>
-      <Link target='_blank' href="https://github.com/guilhermesalviano/messenger-clone">
+      <Link target="_blank" href="https://github.com/guilhermesalviano/messenger-clone">
         <div className="w-full flex max-sm:flex-col flex-row space-x-0 sm:space-x-2">
           <p className="text-neutral-600 dark:text-neutral-400 min-w-20 tabular-nums">
-            {formatDate('2020-08-14', false)}
+            {formatDate("2020-08-14", false)}
           </p>
           <p className="text-neutral-900 dark:text-neutral-100 tracking-tight">
             Messenger Clone for Mobile
           </p>
         </div>
       </Link>
-      <Link target='_blank' href="https://github.com/guilhermesalviano/nlw2-Proffy">
+      <Link target="_blank" href="https://github.com/guilhermesalviano/nlw2-Proffy">
         <div className="w-full flex max-sm:flex-col flex-row space-x-0 sm:space-x-2">
           <p className="text-neutral-600 dark:text-neutral-400 min-w-20 tabular-nums">
-            {formatDate('2020-08-11', false)}
+            {formatDate("2020-08-11", false)}
           </p>
           <p className="text-neutral-900 dark:text-neutral-100 tracking-tight">
             Rocketseat - Proffy
           </p>
         </div>
       </Link>
-      <Link target='_blank' href="https://github.com/guilhermesalviano/ecoleta">
+      <Link target="_blank" href="https://github.com/guilhermesalviano/ecoleta">
         <div className="w-full flex max-sm:flex-col flex-row space-x-0 sm:space-x-2">
           <p className="text-neutral-600 dark:text-neutral-400 min-w-20 tabular-nums">
-            {formatDate('2020-06-02', false)}
+            {formatDate("2020-06-02", false)}
           </p>
           <p className="text-neutral-900 dark:text-neutral-100 tracking-tight">
             Rocketseat - Ecoleta
           </p>
         </div>
       </Link>
-      <Link target='_blank' href="https://github.com/guilhermesalviano/to-be-hero">
+      <Link target="_blank" href="https://github.com/guilhermesalviano/to-be-hero">
         <div className="w-full flex max-sm:flex-col flex-row space-x-0 sm:space-x-2">
           <p className="text-neutral-600 dark:text-neutral-400 min-w-20 tabular-nums">
-            {formatDate('2020-03-27', false)}
+            {formatDate("2020-03-27", false)}
           </p>
           <p className="text-neutral-900 dark:text-neutral-100 tracking-tight">
             Rocketseat - To be hero
