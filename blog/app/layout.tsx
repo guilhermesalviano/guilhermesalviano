c278bf293f86802d1a826e9ee274b44ffa8d8482
Guilherme <guilherme.salviano12@outlook.com>
2026-03-24 19:57:18 -0300

    fix: deps

--- blog/app/layout.tsx
+++ blog/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from 'next'
-import { GeistSans } from 'geist/font/sans'
-import { GeistMono } from 'geist/font/mono'
+import { Geist, Geist_Mono } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/next'
 import { baseUrl } from './sitemap'
